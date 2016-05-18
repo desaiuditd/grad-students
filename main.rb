9b6f2f0db15eb9384a8cb41fb69ad1288155f61e
Udit Desai <desaiuditd@gmail.com>
2016-05-18 15:34:57 -0700

    fixes scss, fixes 404

--- main.rb
+++ main.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'sass'
 
 get '/' do
   erb :index
+end
+
+get '/css/:styles.css' do |styles|
+  scss :"scss/#{styles}"
 end
 
 not_found do
