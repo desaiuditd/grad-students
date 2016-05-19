b57341cf86a57a41d79c01024358ea80ee831e9f
Udit Desai <desaiuditd@gmail.com>
2016-05-19 14:41:28 -0700

    adds DataMapper and comment

--- main.rb
+++ main.rb
@@ -1,17 +1,38 @@
+# include sinatra library
 require 'sinatra'
+# include autoloader
 require 'sinatra/reloader' if development?
+# include scss library
 require 'sass'
+# include student related routes & model
+require './student'
 
-enable :sessions
+# setup Session variables for Login authentication
+configure do
+  enable :sessions
 
-use Rack::Session::Pool, :expire_after => 365 * 24 * 60 * 60 # expire after 1 year in seconds
+  use Rack::Session::Pool, :expire_after => 365 * 24 * 60 * 60 # expire after 1 year in seconds
 
-set :session_secret, 'super grad students secret'
+  set :session_secret, 'super grad students secret'
+end
 
+# define db for development environment
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+# define db for production (heroku) environment
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://#{Dir.pwd}/production.db')
+end
+
+# define SCSS template to compile css
 get '/css/:styles.css' do |styles|
   scss :"scss/#{styles}"
 end
 
+# before filter to check for current logged in user
+# if not logged in then redirect to login page
 before do
   # print method - POST / GET etc.
   puts request.request_method
@@ -25,14 +46,17 @@ before do
   end
 end
 
+# Home Page route & template
 get '/' do
   erb :index
 end
 
+# login page route & template
 get '/login' do
   erb :login
 end
 
+# post method to process login
 post '/login' do
   email = params[:email]
   password = params[:password]
@@ -47,11 +71,13 @@ post '/login' do
 
 end
 
+# logout route
 get '/logout' do
   session[:current_user_auth] = nil
   redirect '/login'
 end
 
+# define 404 not found route & template
 not_found do
   erb :_404
 end
