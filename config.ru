8ea977894efee1a2eb6c0cf494e248d2deb22122
Udit Desai <desaiuditd@gmail.com>
2016-05-21 18:57:08 -0700

    adds comment

--- config.ru
+++ config.ru
@@ -1,3 +1,5 @@
+# Config file for Heroku to start the app.
+
 require './main'
 
 run Sinatra::Application
--- student.rb
+++ student.rb
@@ -22,41 +22,50 @@ class Student
 
 end
 
+# commit the changes for DB.
 DataMapper.finalize
 
+# upgrade the database for new changes
 DataMapper.auto_upgrade!
 
+# list all students route
 get '/students' do
   @students = Student.all
   erb :students
 end
 
+# create new student route
 post '/students' do
   @student = Student.create(params[:student])
   redirect to('/students')
 end
 
+# route for the form to create new student
 get '/students/new' do
   @student = Student.new
   erb :new_student
 end
 
+# show single student route
 get '/students/:id' do
   @student = Student.get(params[:id])
   erb :show_student
 end
 
+# edit single student route
 put '/students/:id' do
   @student = Student.get(params[:id])
   @student.update(params[:student])
   redirect to('/students/'+params[:id])
 end
 
+# delete single student route
 delete '/students/:id' do
   Student.get(params[:id]).destroy
   redirect to('/students')
 end
 
+# route for the form to edit single student
 get '/students/:id/edit' do
   @student = Student.get(params[:id])
   erb :edit_student
